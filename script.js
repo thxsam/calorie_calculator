1def617c751d5877926df7634b7fad4a4f39f868
Samuel Bakhshaei <thxsam@gmail.com>
2023-04-13 00:54:53 +0200

    Add a pie chart
    Add a pie chart + css

--- script.js
+++ script.js
@@ -1,78 +1,113 @@
 
-
-////#
-
-
-
 const caloriesForm = document.getElementById('calories-form');
 caloriesForm.addEventListener('submit', function(event) {
-  event.preventDefault();
-  const gender = document.getElementById('gender').value;
-  const age = parseInt(document.getElementById('age').value);
-  const height = parseInt(document.getElementById('height').value);
-  const weight = parseInt(document.getElementById('weight').value);
-  const activityLevel = parseFloat(document.getElementById('activity-level').value);
-  const strategy = document.getElementById('strategy-selector').value;
+    event.preventDefault();
+    const gender = document.getElementById('gender').value;
+    const age = parseInt(document.getElementById('age').value);
+    const height = parseInt(document.getElementById('height').value);
+    const weight = parseInt(document.getElementById('weight').value);
+    const activityLevel = parseFloat(document.getElementById('activity-level').value);
+    const strategy = document.getElementById('strategy-selector').value;
 
-  let calories = 0;
-  if (gender === 'male') {
-    calories = Math.round((10 * weight) + (6.25 * height) - (5 * age) + 5);
-  } else {
-    calories = Math.round((10 * weight) + (6.25 * height) - (5 * age) - 161);
-  }
+    let calories = 0;
+    if (gender === 'male') {
+        calories = Math.round((10 * weight) + (6.25 * height) - (5 * age) + 5);
+    } else {
+        calories = Math.round((10 * weight) + (6.25 * height) - (5 * age) - 161);
+    }
 
-  calories *= activityLevel;
+    calories *= activityLevel;
 
-  let protein = Math.round(weight * 2);
-  let fat = Math.round(calories * 0.2 / 9);
-  let carbs = Math.round((calories - (protein * 4) - (fat * 9)) / 4);
+    let protein = Math.round(weight * 2);
+    let fat = Math.round(calories * 0.2 / 9);
+    let carbs = Math.round((calories - (protein * 4) - (fat * 9)) / 4);
 
-  if (strategy === 'cutting') {
-    calories = calories * 0.8;
-    protein = Math.round(weight * 2);
-    fat = Math.round(calories * 0.2 / 9);
-    carbs = Math.round((calories - (protein * 4) - (fat * 9)) / 4);
-  } else if (strategy === 'bulking') {
-    calories = calories * 1.2;
-    protein = Math.round(weight * 2.2);
-    fat = Math.round(calories * 0.2 / 9);
-    carbs = Math.round((calories - (protein * 4) - (fat * 9)) / 4);
-  } else {
-    protein = Math.round(weight * 2);
-    fat = Math.round(calories * 0.2 / 9);
-    carbs = Math.round((calories - (protein * 4) - (fat * 9)) / 4);
-  }
+    if (strategy === 'cutting') {
+        calories = calories * 0.8;
+        protein = Math.round(weight * 2);
+        fat = Math.round(calories * 0.2 / 9);
+        carbs = Math.round((calories - (protein * 4) - (fat * 9)) / 4);
+    } else if (strategy === 'bulking') {
+        calories = calories * 1.2;
+        protein = Math.round(weight * 2.2);
+        fat = Math.round(calories * 0.2 / 9);
+        carbs = Math.round((calories - (protein * 4) - (fat * 9)) / 4);
+    } else {
+        protein = Math.round(weight * 2);
+        fat = Math.round(calories * 0.2 / 9);
+        carbs = Math.round((calories - (protein * 4) - (fat * 9)) / 4);
+    }
 
-  const table = document.createElement('table');
-  table.innerHTML = `
-    <tr>
-      <th>Macronutriments</th>
-      <th>Pourcentage</th>
-      <th>Grammes</th>
-      <th>kcal</th>
-    </tr>
-    <tr>
-      <td>Protéines</td>
-      <td>${Math.round(protein * 4 / calories * 100)}%</td>
-      <td>${protein} g</td>
-      <td>${protein * 4} kcal</td>
-    </tr>
-    <tr>
-      <td>Glucides</td>
-      <td>${Math.round(carbs * 4 / calories * 100)}%</td>
-      <td>${carbs} g</td>
-      <td>${carbs * 4} kcal</td>
-    </tr>
-    <tr>
-      <td>Lipides</td>
-      <td>${Math.round(fat * 9 / calories * 100)}%</td>
-      <td>${fat} g</td>
-      <td>${fat * 9} kcal</td>
-    </tr>
-  `;
-  
-  
-  const result = document.getElementById('result');
-  result.innerHTML = `Votre objectif calorique est de <strong>${calories.toFixed(0)} </strong> par jour. <br>Voici ci-dessous la répartition des macronutriments que vous devez suivre : <br>`;
-  result.appendChild(table);
-});
+    const table = document.createElement('table');
+    table.innerHTML = `
+        <tr>
+        <th>Macronutriments</th>
+        <th>Pourcentage</th>
+        <th>Grammes</th>
+        <th>kcal</th>
+        </tr>
+        <tr>
+        <td>Protéines</td>
+        <td>${Math.round(protein * 4 / calories * 100)}%</td>
+        <td>${protein} g</td>
+        <td>${protein * 4} kcal</td>
+        </tr>
+        <tr>
+        <td>Glucides</td>
+        <td>${Math.round(carbs * 4 / calories * 100)}%</td>
+        <td>${carbs} g</td>
+        <td>${carbs * 4} kcal</td>
+        </tr>
+        <tr>
+        <td>Lipides</td>
+        <td>${Math.round(fat * 9 / calories * 100)}%</td>
+        <td>${fat} g</td>
+        <td>${fat * 9} kcal</td>
+        </tr>
+    `;
+    
+    
+    const result = document.getElementById('result');
+    result.innerHTML = `Votre objectif calorique est de <strong>${calories.toFixed(0)} </strong> par jour. <br>Voici ci-dessous la répartition des macronutriments que vous devez suivre : <br>`;
+    result.appendChild(table);
+
+
+    // Adding a chartpie
+    const chartCanvas = document.createElement('canvas');
+    const chartContext = chartCanvas.getContext('2d');
+
+    const chartData = {
+    datasets: [{
+        data: [protein, carbs, fat],
+        backgroundColor: ['#ff6384', '#36a2eb', '#ffce56'],
+    }],
+    labels: ['Protéines (g)', 'Glucides (g)', 'Lipides (g)']
+    };
+
+    const chartOptions = {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          title: {
+            display: true,
+            text: 'Répartition des macronutriments sur la journée',
+            fontSize: 20
+          }
+        }
+      };
+      
+    const chart = new Chart(chartContext, {
+    type: 'pie',
+    data: chartData,
+    options: chartOptions
+    });
+    
+    const chartContainer = document.createElement('div');
+    chartContainer.classList.add('chart-container');
+    chartContainer.style.width = '450px'; // adjust width as needed
+    chartContainer.style.height = '450px'; // adjust height as needed
+    chartContainer.appendChild(chartCanvas);
+    
+    result.appendChild(chartContainer);
+})
+
